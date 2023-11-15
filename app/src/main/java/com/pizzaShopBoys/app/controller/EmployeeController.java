1452bc6642dbce04b0da7d87ae10b0646d556ba3
netanelfrankel <netanel.frankel@fiserv.com>
2023-11-14 22:09:19 -0500

    added employee controller

---
+++ app/src/main/java/com/pizzaShopBoys/app/controller/EmployeeController.java
@@ -0,0 +1,40 @@
+package com.pizzaShopBoys.app.controller;
+
+import com.pizzaShopBoys.app.model.Employee;
+import com.pizzaShopBoys.app.model.Product;
+import com.pizzaShopBoys.app.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+    @Autowired
+    EmployeeService employeeService;
+    @GetMapping("/employees")
+    ResponseEntity<List<Employee>> getAllEmployees(){
+        return ResponseEntity.status(HttpStatus.OK).body(employeeService.getAllEmployees());
+    }
+
+    @PostMapping("/employees")
+    ResponseEntity<Employee> createEmployee(@RequestBody Employee employee){
+        return employeeService.createEmployee(employee)
+                .map(ResponseEntity::ok)
+                .orElseGet(()->ResponseEntity.internalServerError().build());
+    }
+
+    @PutMapping("/employees/{id}")
+    ResponseEntity<Employee> updateProduct(@RequestBody Employee employee){
+        return employeeService.updateEmployee(employee).map(ResponseEntity::ok)
+                .orElseGet(()->ResponseEntity.notFound().build());
+    }
+
+    ResponseEntity<Void> deleteEmployeeById(@PathVariable int id){
+        employeeService.deleteEmployeeById(id);
+        return ResponseEntity.noContent().build();
+    }}
--- app/src/main/java/com/pizzaShopBoys/app/repository/EmployeeRepository.java
+++ app/src/main/java/com/pizzaShopBoys/app/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.pizzaShopBoys.app.repository;
 
 import com.pizzaShopBoys.app.model.Employee;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends ListCrudRepository<Employee,Integer> {
 }
