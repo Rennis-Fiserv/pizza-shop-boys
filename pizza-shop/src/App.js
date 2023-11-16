4f2e781334f33d9197303c34fd51162be38f25a3
F93AB7X <joseph.schmitt1@fiserv.com>
2023-11-16 14:28:24 -0500

    Products, PizzaShopAdmin fleshed out

--- pizza-shop/src/App.js
+++ pizza-shop/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './App.css';
-import Home from './Home';
+import Home from './components/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import EmployeeInfo from './EmployeeInfo';
-import EmployeeEdit from './EmployeeEdit';
-import EmployeeAdd from './EmployeeAdd';
+import EmployeeInfo from './components/EmployeeInfo';
+import EmployeeEdit from './components/EmployeeEdit';
+import EmployeeAdd from './components/EmployeeAdd';
+import PizzaShopAdmin from './components/PizzaShopAdmin';
+import Products from './components/Products';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
+        <Route path='/pizzashopadmin' element={<PizzaShopAdmin/>}/>
         <Route path='/employees' exact={true} element={<EmployeeInfo/>}/>
         <Route path='/employees/:id' exact={true} element={<EmployeeEdit/>}/>
         <Route path='/employees/new' element={<EmployeeAdd/>}/>
+        <Route path='/products' element={<Products/>}/>
       </Routes>
     </Router>
   )
