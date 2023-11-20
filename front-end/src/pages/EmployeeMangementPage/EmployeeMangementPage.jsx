949423b3ce499df624ceb74bf8ab48b1d1bf23e6
F93AB7X <joseph.schmitt1@fiserv.com>
2023-11-20 14:58:49 -0500

    Add functionality complete

--- front-end/src/pages/EmployeeMangementPage/EmployeeMangementPage.jsx
+++ front-end/src/pages/EmployeeMangementPage/EmployeeMangementPage.jsx
@@ -1,18 +1,23 @@
 import { useEffect, useState } from "react";
 
 const EmployeeManagementPage = () => {
-  
+  const initialFormState = {
+    id: "",
+    firstName: "",
+    lastName: "",
+    salary: "",
+    employeeRole: "",
+  };
+  const [employee, setEmployee] = useState(initialFormState);
   const [employees, setEmployees] = useState([]);
   const [loading, setLoading] = useState(false);
-
-
 
   useEffect(() => {
     setLoading(true);
 
-    fetch('http://localhost:8080/api/employees')
-      .then(response => response.json()) 
-      .then(data => {
+    fetch("http://localhost:8080/api/employees")
+      .then((response) => response.json())
+      .then((data) => {
         setEmployees(data);
         setLoading(false);
       });
@@ -22,8 +27,8 @@ const EmployeeManagementPage = () => {
     await fetch(`http://localhost:8080/api/employees/${id}`, {
       method: "DELETE",
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
     }).then(() => {
       let updatedEmployees = [...employees].filter((i) => i.id !== id);
@@ -31,78 +36,96 @@ const EmployeeManagementPage = () => {
     });
   };
 
-    if (loading) {
-      return <p>Loading...</p>;
-    }
-  
-  const employeesList = employees.map(employee => {
-    return <form key={employee.id} onSubmit={(e) => handleSubmit(e, employee.id)}>
-    <table>
-      <thead>
-        <tr>
-          <td style={{ whiteSpace: "nowrap" }} width="20%">
-            <input
-              type="text"
-              name="firstName"
-              value={employee.firstName}
-              onChange={(e) => handleEmployeeFieldChange(e, employee.id)}
-            />
-          </td>
-          <td width="20%">
-            <input
-              type="text"
-              name="lastName"
-              value={employee.lastName}
-              onChange={(e) => handleEmployeeFieldChange(e, employee.id)}
-            />
-          </td>
-          <td width="20%">
-            <input
-              type="text"
-              name="salary"
-              value={employee.salary}
-              onChange={(e) => handleEmployeeFieldChange(e, employee.id)}
-            />
-          </td>
-          <td width="20%">
-            <input
-              type="text"
-              name="employeeRole"
-              value={employee.employeeRole}
-              onChange={(e) => handleEmployeeFieldChange(e, employee.id)}
-            />
-          </td>
-          <td>
-            <button size="sm" color="primary" type="submit">
-              Save
-            </button>
-            <button
-              size="sm"
-              color="danger"
-              onClick={() => remove(employee.id)}
-            >
-              Delete
-            </button>
-            <button
-              size="sm"
-              color="danger"
-              onClick={() => remove(employee.id)}
-            >
-              Delete
-            </button>
-          </td>
-        </tr>
-      </thead>
-    </table>
-  </form>
+  const add = async (event) => {
+    event.preventDefault();
+
+    await fetch(`http://localhost:8080/api/employees`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(employee),
+    });
+    setEmployee(initialFormState);
+    // Reload the data after adding a new employee
+    fetchEmployees();
+  };
+
+  const fetchEmployees = () => {
+    fetch("http://localhost:8080/api/employees")
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployees(data);
+      });
+  };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const employeesList = employees.map((employee) => {
+    return (
+      <form key={employee.id} onSubmit={(e) => handleSubmit(e, employee.id)}>
+        <table>
+          <thead>
+            <tr>
+              <td style={{ whiteSpace: "nowrap" }} width="20%">
+                <input
+                  type="text"
+                  name="firstName"
+                  value={employee.firstName}
+                  onChange={(e) => handleEmployeeFieldChange(e, employee.id)}
+                />
+              </td>
+              <td width="20%">
+                <input
+                  type="text"
+                  name="lastName"
+                  value={employee.lastName}
+                  onChange={(e) => handleEmployeeFieldChange(e, employee.id)}
+                />
+              </td>
+              <td width="20%">
+                <input
+                  type="text"
+                  name="salary"
+                  value={employee.salary}
+                  onChange={(e) => handleEmployeeFieldChange(e, employee.id)}
+                />
+              </td>
+              <td width="20%">
+                <input
+                  type="text"
+                  name="employeeRole"
+                  value={employee.employeeRole}
+                  onChange={(e) => handleEmployeeFieldChange(e, employee.id)}
+                />
+              </td>
+              <td>
+                <button size="sm" color="primary" type="submit">
+                  Save
+                </button>
+                <button
+                  size="sm"
+                  color="danger"
+                  onClick={() => remove(employee.id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </thead>
+        </table>
+      </form>
+    );
   });
+
 
   const handleEmployeeFieldChange = (event, employeeId) => {
     const { name, value } = event.target;
     const updatedEmployees = employees.map((employee) =>
-      employee.id === employeeId
-        ? { ...employee, [name]: value }
-        : employee
+      employee.id === employeeId ? { ...employee, [name]: value } : employee
     );
     setEmployees(updatedEmployees);
   };
@@ -117,19 +140,16 @@ const EmployeeManagementPage = () => {
     await fetch(`http://localhost:8080/api/employees/${employeeId}`, {
       method: "PUT",
       headers: {
-        "Accept": "application/json",
+        Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify(editedEmployee),
     });
   };
 
-
   return (
     <>
       <div>
-        <div className="float-end">
-        </div>
         <h3>Employees</h3>
         <table>
           <thead>
@@ -142,6 +162,56 @@ const EmployeeManagementPage = () => {
           </thead>
         </table>
         <div>{employeesList}</div>
+        {/* New employee form */}
+        <form onSubmit={add}>
+          <table>
+            <thead>
+              <tr>
+                <td>
+                  <input
+                    type="text"
+                    name="firstName"
+                    value={employee.firstName}
+                    onChange={(e) => setEmployee({ ...employee, firstName: e.target.value })}
+                    placeholder="First Name"
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    name="lastName"
+                    value={employee.lastName}
+                    onChange={(e) => setEmployee({ ...employee, lastName: e.target.value })}
+                    placeholder="Last Name"
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    name="salary"
+                    value={employee.salary}
+                    onChange={(e) => setEmployee({ ...employee, salary: e.target.value })}
+                    placeholder="Salary"
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    name="employeeRole"
+                    value={employee.employeeRole}
+                    onChange={(e) => setEmployee({ ...employee, employeeRole: e.target.value })}
+                    placeholder="Role"
+                  />
+                </td>
+                <td>
+                  <button size="sm" color="success" type="submit">
+                    Add
+                  </button>
+                </td>
+              </tr>
+            </thead>
+          </table>
+        </form>
       </div>
     </>
   );
