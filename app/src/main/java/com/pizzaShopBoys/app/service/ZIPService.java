03141ab9890c9e96885e0f3dc830470707408bd3
Drew-Huth <147175635+Drew-Huth@users.noreply.github.com>
2023-11-21 18:29:04 +0000

    EmployeeController and ZIPService update

--- app/src/main/java/com/pizzaShopBoys/app/service/ZIPService.java
+++ app/src/main/java/com/pizzaShopBoys/app/service/ZIPService.java
@@ -1,8 +1,6 @@
 package com.pizzaShopBoys.app.service;
 
 import com.pizzaShopBoys.app.model.ZIP;
-import com.pizzaShopBoys.app.model.ZIP;
-import com.pizzaShopBoys.app.repository.ZIPRepository;
 import com.pizzaShopBoys.app.repository.ZIPRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,35 +11,36 @@ import java.util.Optional;
 public class ZIPService {
     @Autowired
 
-     ZIPRepository zip_Repository;
+    ZIPRepository zip_Repository;
 
-//    CREATE
-    public Optional<ZIP> createZIP(ZIP ZIP){
-    return Optional.of(zip_Repository.save(ZIP));
-}
+    // CREATE
+    public Optional<ZIP> createZIP(ZIP ZIP) {
+        return Optional.of(zip_Repository.save(ZIP));
+    }
 
-//    READ
-    public List<ZIP> getAllZIPs(){
-    return zip_Repository.findAll();
-}
-    public Optional<ZIP> getZIPById(int id){
+    // READ
+    public List<ZIP> getAllZIPs() {
+        return zip_Repository.findAll();
+    }
+
+    public Optional<ZIP> getZIPById(int id) {
         return zip_Repository.findById(id);
     }
 
-//    UPDATE
-    public Optional<ZIP> updateZIP(ZIP ZIP){
-        if(zip_Repository.existsById(ZIP.getId())){
+    // UPDATE
+    public Optional<ZIP> updateZIP(ZIP ZIP) {
+        if (zip_Repository.existsById(ZIP.getId())) {
             return Optional.of(zip_Repository.save(ZIP));
-        }
-        else return Optional.empty();
+        } else
+            return Optional.empty();
     }
 
-//    DELETE
-    public void deleteZIPById(int id){
+    // DELETE
+    public void deleteZIPById(int id) {
         zip_Repository.deleteById(id);
     }
 
-    public void deleteAllZIP(){
+    public void deleteAllZIP() {
         zip_Repository.deleteAll();
     }
 
