30dc86e90d3c3846e65d2582f3ffa5321da3a2a7
netanelfrankel <netanel.frankel@fiserv.com>
2023-11-22 15:02:15 -0500

    deleted front end 2 folder

--- app/src/main/java/com/pizzaShopBoys/app/controller/CustomerController.java
+++ app/src/main/java/com/pizzaShopBoys/app/controller/CustomerController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
