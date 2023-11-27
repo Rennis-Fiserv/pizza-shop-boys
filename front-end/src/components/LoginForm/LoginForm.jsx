3be1dba9fc81c03e57b62395545280b753fdbc98
F93AB7X <joseph.schmitt1@fiserv.com>
2023-11-27 16:43:05 -0500

    changes login

--- front-end/src/components/LoginForm/LoginForm.jsx
+++ front-end/src/components/LoginForm/LoginForm.jsx
@@ -3,29 +3,30 @@ import fetchEmployee from "../../util/requests/fetchEmployee"
 import { useContext } from "react"
 import {UserContext} from '../../contexts/UserContext'
 import { useNavigate } from "react-router-dom"
-
 import './LoginForm.css'
-
+ 
 export default function LoginForm(){
 const {user, loginUser, logoutUser }= useContext(UserContext)
 const navigate = useNavigate()
-
+ 
     const {
         register,
         handleSubmit,
         formState: { errors },
       } = useForm()
-
+ 
     async function loginFormHandler(data){
         const response = await fetchEmployee(data['id'])
         loginUser(response)
-        if(response['employeeRole'] == 'E') navigate('/employee/orders')
+        if(response['employeeRole'] == 'E') navigate('/employee')
         if (response ['employeeRole'] == 'M') navigate('/manager')
     }
     return (
-        <form onSubmit={handleSubmit(loginFormHandler)}>
-            <input type="number" placeholder="12345" {...register("id", {required: true})} />
-            <input type="submit" value="login"/>
+        <form className="logform" onSubmit={handleSubmit(loginFormHandler)}>
+            <div className="log-headding">{'Enter Employee ID:'}</div>
+            <input className="login-input" type="number" placeholder="Employee ID" {...register("id", {required: true})} />
+            <div className="space"></div>
+            <input className="logbttn" type="submit" value="Enter"/>
         </form>
     )
 }
--- front-end/src/pages/LoginPage/LoginPage.jsx
+++ front-end/src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,19 @@
 import Card from "../../components/Card/Card";
 import LoginForm from "../../components/LoginForm/LoginForm";
-
+import './LoginPage.css'
+import image from '../../images/transparentpizzashopboys.png'
+ 
 export default function LoginPage(){
     return(
-        <>
-        <Card className="login-card">
-        <LoginForm/>
-        </Card>
-        </>
+        <div className="logpage">
+            <Card className="login-card">
+                <div className="photo">
+                    <img className="logo"src={image}></img>
+                </div>
+                <div>
+                    <LoginForm/>
+                </div>
+            </Card>
+        </div>
     )
 }
