756d3a576f60814a3a0a78a1af20f92654022b81
F93AB7X <joseph.schmitt1@fiserv.com>
2023-11-27 14:21:41 -0500

    formatted routes.jsx

--- front-end/src/routes/Routes.jsx
+++ front-end/src/routes/Routes.jsx
@@ -1,49 +1,44 @@
-import {createBrowserRouter,createRoutesFromElements,RouterProvider, Route} from "react-router-dom"
-import EmployeePage from "../pages/EmployeePage/EmployeePage"
-import LoginPage from "../pages/LoginPage/LoginPage"
-import ManagerPage from "../ManagerPage"
-import RootLayout from "../pages/RootLayout"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import EmployeePage from "../pages/EmployeePage/EmployeePage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import ManagerPage from "../ManagerPage";
+import RootLayout from "../pages/RootLayout";
 
-import EmployeeManagementPage from "../pages/EmployeeMangementPage/EmployeeMangementPage"
-import PizzaShopStatsPage from "../pages/PizzaShopStatsPage/PizzaShopStatsPage"
+import EmployeeManagementPage from "../pages/EmployeeMangementPage/EmployeeMangementPage";
+import PizzaShopStatsPage from "../pages/PizzaShopStatsPage/PizzaShopStatsPage";
 
-import OrderPage from "../pages/OrderPage/OrderPage"
-
-
+import OrderPage from "../pages/OrderPage/OrderPage";
 
 const routes = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<RootLayout/>}>
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<LoginPage />}></Route>
 
-        <Route index element={<LoginPage/>}></Route>
-        
-        <Route path="employee" element={<EmployeePage/>}>
-        <Route path="orders" element={<OrderPage/>}></Route>
-        </Route>
-        
-        <Route path="manager" element={<ManagerPage/>}>
+      <Route path="employee" element={<EmployeePage />}>
+        <Route path="orders" element={<OrderPage />}></Route>
+      </Route>
 
+      <Route path="manager" element={<ManagerPage />}>
+        <Route path="orders" element={<OrderPage />}></Route>
+        <Route
+          path="employee-management"
+          element={<EmployeeManagementPage />}
+        ></Route>
+        <Route path="shop-metrics" element={<PizzaShopStatsPage />}></Route>
+      </Route>
+    </Route>
+  )
+);
 
-            <Route path="orders" element={<OrderPage/>}></Route>
-            <Route path="employee-management" element={<EmployeeManagementPage/>}></Route>
-            <Route path="shop-metrics" element={<PizzaShopStatsPage/>}></Route>
-            
-
-            <Route path="orders" element={<OrderPage/>}></Route>
-            <Route path="employee-management" element={<p>employee-management</p>}></Route>
-            <Route path="shop-metrics" element={<p>employee-metrics</p>}></Route>
-        
-
-        
-        </Route> 
-        </Route>
-    )
-)
-
-export default function Routes (){
-    return(
-        <>
-        <RouterProvider router={routes}/>
-        </>
-    )
+export default function Routes() {
+  return (
+    <>
+      <RouterProvider router={routes} />
+    </>
+  );
 }
